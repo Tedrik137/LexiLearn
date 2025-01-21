944f75f7ed1e3596944c0d43ed9d6d13993a8666
Ricardo Akkari <softwareengricky@outlook.com>
2025-01-21 12:15:53 +1100

    cleaned tabs

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,13 +1,9 @@
-import { StyleSheet, Image, Platform } from "react-native";
+import { StyleSheet } from "react-native";
 
-import { Collapsible } from "@/components/Collapsible";
-import { ExternalLink } from "@/components/ExternalLink";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
 import { IconSymbol } from "@/components/ui/IconSymbol";
 
-export default function TabTwoScreen() {
+export default function AccountScreen() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
